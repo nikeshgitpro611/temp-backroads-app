08823aba8e8e3bb0aecbfaea557b8e7759e7ee9f
nikeshgitpro611 <112698036+nikeshgitpro611@users.noreply.github.com>
2023-05-31 23:52:02 +0530

    update

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="nav-center">
         <div className="nav-header">
-          <img src={logo} className="nav-logo" />
+          <img src={logo} className="nav-logo" alt='backroads'/>
           <button type="button" className="nav-toggle" id="nav-toggle">
             <i className="fas fa-bars"></i>
           </button>
