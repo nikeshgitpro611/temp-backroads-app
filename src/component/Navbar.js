7389cd04a20748cde0bd766614d957a2479a9a1f
nikeshgitpro611 <112698036+nikeshgitpro611@users.noreply.github.com>
2023-05-31 23:58:09 +0530

    update4th

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,46 +1,29 @@
 import React from "react";
 import logo from "../images/logo.svg";
 import { pageLinks, socialLinks } from "../Data/data";
-
+import SocialLink from './SocilaLink'
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="nav-center">
-        <div className="nav-header">
-          <img src={logo} className="nav-logo" alt='backroads'/>
-          <button type="button" className="nav-toggle" id="nav-toggle">
-            <i className="fas fa-bars"></i>
+    <nav className='navbar'>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} className='nav-logo' alt='backroads' />
+          <button type='button' className='nav-toggle' id='nav-toggle'>
+            <i className='fas fa-bars'></i>
           </button>
         </div>
-        {/* <!-- Center Header --> */}
-        <ul className="nav-links" id="nav-links">
-          {pageLinks.map((link) => {
-            return (
-              <li key={link.id}>
-                <a href={link.href} className="nav-link">
-                  {link.text}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
+        {/* <!-- left this comment on purpose --> */}
 
-        {/* <!-- Right Content --> */}
-        <ul className="nav-icons">
+        <PageLinks parentClass='nav-links' itemClass='nav-link' />
+
+        <ul className='nav-icons'>
           {socialLinks.map((link) => {
-            const { id, href, icon } = link;
-            return (
-              <li key={id}>
-                <a href={href} target="_blank" className="nav-icon">
-                  <i className={icon}></i>
-                </a>
-              </li>
-            );
+            return <SocialLink {...link} key={link.id} itemClass='nav-icon' />
           })}
         </ul>
       </div>
     </nav>
-  );
+  )
 };
 
 export default Navbar;
